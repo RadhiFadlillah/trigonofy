dba5f8b101f0d0dc23e026119c805cd46793c992
Radhi Fadlillah <m.radhi.f@gmail.com>
2021-09-25 07:45:30 +0700

    Improve build script

--- build.esbuild.mjs
+++ build.esbuild.mjs
@@ -6,31 +6,44 @@ const devMode = args.includes('dev');
 const serveMode = args.includes('serve');
 
 // Prepare options
-const serveOpts = {
+const serveOptions = {
 	port: 9000,
 	servedir: '.',
 };
 
-const buildOpts = {
-	entryPoints: ['./src/index.ts'],
-	outfile: devMode ? 'trigonofy.js' : 'trigonofy.min.js',
-	minify: !devMode,
+const buildOptions = {
 	bundle: true,
-	watch: devMode && !serveMode,
-	sourcemap: devMode,
+	sourcemap: true,
 	treeShaking: true,
-	globalName: 'Trigonofy',
+	globalName: 'trigonofy',
+	outfile: 'trigonofy.js',
+	entryPoints: ['./src/index.ts'],
+};
+
+const devBuildOptions = {
+	...buildOptions,
+	watch: !serveMode,
+};
+
+const bundleProdBuildOptions = {
+	...buildOptions,
+	minify: true,
+	sourcemap: false,
+	outfile: 'trigonofy-bundle.min.js',
+};
+
+const prodBuildOptions = {
+	...bundleProdBuildOptions,
+	outfile: 'trigonofy.min.js',
+	external: ['chroma-js', 'd3-delaunay'],
 };
 
 // Run esbuild
 if (serveMode) {
-	serve(serveOpts, buildOpts).catch((err) => {
-		console.error(err);
-		process.exit(1);
-	});
+	serve(serveOptions, buildOptions);
+} else if (devMode) {
+	build(devBuildOptions);
 } else {
-	build(buildOpts).catch((err) => {
-		console.error(err);
-		process.exit(1);
-	});
+	build(prodBuildOptions);
+	build(prodBundleBuildOptions);
 }
