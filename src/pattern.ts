0f11a5cb70c03c5265b720768cabd8b2bd19a2a6
Radhi Fadlillah <m.radhi.f@gmail.com>
2021-09-26 13:49:45 +0700

    Restructure API

--- src/pattern.ts
+++ src/pattern.ts
@@ -16,105 +16,67 @@ export interface PatternOptions {
 export class Pattern {
 	width: number;
 	height: number;
-	delaunay: Delaunay<Delaunay.Point>;
-	voronoi: Voronoi<Delaunay.Point>;
+	points: Point[];
+	palette: string[];
+	angle: number;
 
-	constructor({
-		width = 800,
-		height = 600,
-		points = [],
-		palette = [],
-		gradientAngle = undefined,
-	}: PatternOptions = {}) {
-		// Process arguments
-		this.width = width;
-		this.height = height;
+	constructor(opts: PatternOptions = {}) {
+		opts = validateOptions(opts);
+		this.width = opts.width;
+		this.height = opts.height;
+		this.points = opts.points;
+		this.palette = opts.palette;
+		this.angle = opts.gradientAngle;
+	}
 
-		if (!validatePoints(points)) {
-			points = Points.random(width, height);
-		}
-
-		if (!validatePalette(palette)) {
-			palette = Palette.random();
-		}
-
-		if (!validateGradientAngle(gradientAngle)) {
-			gradientAngle = Math.round(Math.random() * 360);
-		}
-
+	renderTriangle(): HTMLCanvasElement {
 		// Create canvas
-		const canvas = document.createElement('canvas');
-		canvas.width = this.width;
-		canvas.height = this.height;
-		document.body.appendChild(canvas);
-
-		// Create fill gradient
-		const ctx = canvas.getContext('2d');
-		const fillGradient = createGradient(
-			ctx,
-			width,
-			height,
-			palette,
-			gradientAngle
-		);
-
-		// Create border gradient
-		const paletteLuminance = chroma.average(palette, 'lch').luminance();
-		const strokePalette = palette.map((hex) => {
-			let color = chroma(hex);
-			if (paletteLuminance >= 0.5) color = color.darken();
-			else color = color.brighten();
-			return color.hex();
-		});
-		const strokeGradient = createGradient(
-			ctx,
-			width,
-			height,
-			strokePalette,
-			gradientAngle
-		);
+		const [canvas, ctx] = this._prepareCanvas();
 
 		// Draw background
-		ctx.fillStyle = fillGradient;
-		ctx.fillRect(0, 0, width, height);
+		ctx.fillRect(0, 0, this.width, this.height);
 
 		// Draw delaunay triangles
-		ctx.lineWidth = 1;
-		ctx.strokeStyle = strokeGradient;
+		const delaunay = Delaunay.from(this.points);
+		[...delaunay.trianglePolygons()].forEach((points) => {
+			// Check if this triangle visible
+			let visible = points.some((p) => {
+				let [x, y] = [p[0], p[1]];
+				return x >= 0 && x <= this.width && y >= 0 && y <= this.height;
+			});
 
-		// const delaunay = Delaunay.from(points);
-		// [...delaunay.trianglePolygons()].forEach((points) => {
-		// 	// Check if this triangle visible
-		// 	let visible = points.some((p) => {
-		// 		let [x, y] = [p[0], p[1]];
-		// 		return x >= 0 && x <= width && y >= 0 && y <= height;
-		// 	});
+			// Skip invisible triangles
+			if (!visible) return;
 
-		// 	// Skip invisible triangles
-		// 	if (!visible) return;
+			// Calculate centroid
+			let cx = (points[0][0] + points[1][0] + points[2][0]) / 3;
+			if (cx <= 0) cx = 1;
+			else if (cx >= this.width) cx = this.width - 1;
 
-		// 	// Calculate centroid
-		// 	let cx = (points[0][0] + points[1][0] + points[2][0]) / 3;
-		// 	if (cx <= 0) cx = 1;
-		// 	else if (cx >= width) cx = width - 1;
+			let cy = (points[0][1] + points[1][1] + points[2][1]) / 3;
+			if (cy <= 0) cy = 1;
+			else if (cy >= this.height) cy = this.height - 1;
 
-		// 	let cy = (points[0][1] + points[1][1] + points[2][1]) / 3;
-		// 	if (cy <= 0) cy = 1;
-		// 	else if (cy >= height) cy = height - 1;
+			// Draw triangle
+			ctx.fillStyle = getColor(ctx, cx, cy);
+			drawPolygon(ctx, points as Point[]);
+			ctx.fill();
+			ctx.stroke();
+		});
 
-		// 	// Fetch color at centroid
-		// 	ctx.fillStyle = getColor(ctx, cx, cy);
-		// 	ctx.beginPath();
-		// 	ctx.moveTo(points[0][0], points[0][1]);
-		// 	ctx.lineTo(points[1][0], points[1][1]);
-		// 	ctx.lineTo(points[2][0], points[2][1]);
-		// 	ctx.lineTo(points[3][0], points[3][1]);
-		// 	ctx.fill();
-		// 	ctx.stroke();
-		// });
+		return canvas;
+	}
 
-		const delaunay = Delaunay.from(points);
-		const voronoi = delaunay.voronoi([0, 0, width, height]);
+	renderVoronoi(): HTMLCanvasElement {
+		// Create canvas
+		const [canvas, ctx] = this._prepareCanvas();
+
+		// Draw background
+		ctx.fillRect(0, 0, this.width, this.height);
+
+		// Draw voronoi cells
+		const delaunay = Delaunay.from(this.points);
+		const voronoi = delaunay.voronoi([0, 0, this.width, this.height]);
 		[...voronoi.cellPolygons()].forEach((points, i) => {
 			// Calculate centroid
 			let [sumX, sumY] = [0, 0];
@@ -127,42 +89,119 @@ export class Pattern {
 			let cx = sumX / nVertice;
 			let cy = sumY / nVertice;
 
-			// Fetch color at centroid
+			// Draw cell
 			ctx.fillStyle = getColor(ctx, cx, cy);
-			ctx.beginPath();
-			ctx.moveTo(points[0][0], points[0][1]);
-			for (let i = 1; i < points.length; i++) {
-				ctx.lineTo(points[i][0], points[i][1]);
-			}
+			drawPolygon(ctx, points as unknown as Point[]);
 			ctx.fill();
 			ctx.stroke();
 		});
+
+		return canvas;
 	}
 
-	// 	[...delaunay.trianglePolygons()].forEach((points, i) => {
-	// 		// Calculate centroid
-	// 		let cx = (points[0][0] + points[1][0] + points[2][0]) / 3;
-	// 		if (cx <= 0) cx = 1;
-	// 		else if (cx >= width) cx = width - 1;
+	_createGradient(
+		ctx: CanvasRenderingContext2D,
+		palette?: string[]
+	): CanvasGradient {
+		// Prepare variables
+		let { angle, width, height } = { ...this };
+		if (!paletteValid(palette)) {
+			palette = this.palette;
+		}
 
-	// 		let cy = (points[0][1] + points[1][1] + points[2][1]) / 3;
-	// 		if (cy <= 0) cy = 1;
-	// 		else if (cy >= height) cy = height - 1;
+		// Normalize angle
+		if (angle > 180) angle -= 180;
 
-	// 		// Fetch color at circumcenters
-	// 		ctx.fillStyle = getColor(ctx, cx, cy);
-	// 		ctx.beginPath();
-	// 		ctx.moveTo(points[0][0], points[0][1]);
-	// 		ctx.lineTo(points[1][0], points[1][1]);
-	// 		ctx.lineTo(points[2][0], points[2][1]);
-	// 		ctx.lineTo(points[3][0], points[3][1]);
-	// 		ctx.fill();
-	// 		ctx.stroke();
-	// 	});
-	// }
+		// Calculate gradient endpoints
+		let [x0, y0] = [width / 2, 0];
+		let [x1, y1] = [width / 2, height];
+		if (angle !== 90) {
+			// Calculate slope
+			let m = Math.tan((angle * Math.PI) / 180);
+
+			// Since we use center as zero point, for simplicity we will
+			// calculate using quarter of area.
+			let limitX = width / 2;
+			let limitY = height / 2;
+			let [x, y] = [limitX, m * limitX];
+			if (y > limitY) [x, y] = [limitY / m, limitY];
+			else if (y < -limitY) [x, y] = [-limitY / m, -limitY];
+
+			// Create final endpoints
+			[x1, y1] = [Math.round(x + width / 2), Math.round(y + height / 2)];
+			[x0, y0] = [width - x1, height - y1];
+
+			// If slope descending swap the points
+			if (m < 0) {
+				[x0, x1] = [x1, x0];
+				[y0, y1] = [y1, y0];
+			}
+		}
+
+		// Create gradient
+		const paletteSize = palette.length;
+		const gradient = ctx.createLinearGradient(x0, y0, x1, y1);
+		palette.forEach((color, i) => {
+			gradient.addColorStop(i / (paletteSize - 1), color);
+		});
+
+		return gradient;
+	}
+
+	_prepareCanvas(): [HTMLCanvasElement, CanvasRenderingContext2D] {
+		// Create canvas
+		const canvas = document.createElement('canvas');
+		canvas.width = this.width;
+		canvas.height = this.height;
+
+		// Create fill gradient
+		const ctx = canvas.getContext('2d');
+		const fillGradient = this._createGradient(ctx);
+
+		// Create stroke gradient
+		const luminance = chroma.average(this.palette, 'lch').luminance();
+		const strokePalette = this.palette.map((hex) => {
+			let color = chroma(hex);
+			color = luminance >= 0.5 ? color.darken() : color.brighten();
+			return color.hex();
+		});
+		const strokeGradient = this._createGradient(ctx, strokePalette);
+
+		// Set styling
+		ctx.lineWidth = 1;
+		ctx.fillStyle = fillGradient;
+		ctx.strokeStyle = strokeGradient;
+
+		return [canvas, ctx];
+	}
 }
 
-function validatePoints(points?: any): boolean {
+function validateOptions(opts: PatternOptions = {}): PatternOptions {
+	if (typeof opts.width !== 'number') {
+		opts.width = 800;
+	}
+
+	if (typeof opts.height !== 'number') {
+		opts.height = 600;
+	}
+
+	if (!pointsValid(opts.points)) {
+		opts.points = Points.random(opts.width, opts.height);
+	}
+
+	if (!paletteValid(opts.palette)) {
+		opts.palette = Palette.random();
+	}
+
+	if (!angleValid(opts.gradientAngle)) {
+		opts.gradientAngle = randomInt(0, 360);
+	}
+
+	opts.gradientAngle = normalizeAngle(opts.gradientAngle);
+	return opts;
+}
+
+function pointsValid(points?: any): boolean {
 	if (!Array.isArray(points)) return false;
 	if (points.length === 0) return false;
 
@@ -176,7 +215,7 @@ function validatePoints(points?: any): boolean {
 	});
 }
 
-function validatePalette(palette?: any): boolean {
+function paletteValid(palette?: any): boolean {
 	if (!Array.isArray(palette)) return false;
 	if (palette.length < 2) return false;
 
@@ -185,55 +224,8 @@ function validatePalette(palette?: any): boolean {
 	});
 }
 
-function validateGradientAngle(angle?: number): boolean {
+function angleValid(angle?: number): boolean {
 	return typeof angle === 'number' && isFinite(angle);
-}
-
-function createGradient(
-	ctx: CanvasRenderingContext2D,
-	width: number,
-	height: number,
-	palette: string[],
-	angle: number
-): CanvasGradient {
-	// Normalize angle
-	angle = normalizeAngle(angle);
-	if (angle > 180) angle -= 180;
-
-	// Calculate gradient endpoints
-	let [x0, y0] = [width / 2, 0];
-	let [x1, y1] = [width / 2, height];
-	if (angle !== 90) {
-		// Calculate slope
-		let m = Math.tan((angle * Math.PI) / 180);
-
-		// Since we use center as zero point, for simplicity we will
-		// calculate using quarter of area.
-		let limitX = width / 2;
-		let limitY = height / 2;
-		let [x, y] = [limitX, m * limitX];
-		if (y > limitY) [x, y] = [limitY / m, limitY];
-		else if (y < -limitY) [x, y] = [-limitY / m, -limitY];
-
-		// Create final endpoints
-		[x1, y1] = [Math.round(x + width / 2), Math.round(y + height / 2)];
-		[x0, y0] = [width - x1, height - y1];
-
-		// If slope descending swap the points
-		if (m < 0) {
-			[x0, x1] = [x1, x0];
-			[y0, y1] = [y1, y0];
-		}
-	}
-
-	// Create gradient
-	const paletteSize = palette.length;
-	const gradient = ctx.createLinearGradient(x0, y0, x1, y1);
-	palette.forEach((color, i) => {
-		gradient.addColorStop(i / (paletteSize - 1), color);
-	});
-
-	return gradient;
 }
 
 function normalizeAngle(angle: number): number {
@@ -252,4 +244,12 @@ function getColor(ctx: CanvasRenderingContext2D, x: number, y: number): string {
 	const pixel = ctx.getImageData(x, y, 1, 1).data;
 	const color = chroma.rgb(pixel[0], pixel[1], pixel[2]);
 	return color.hex();
+}
+
+function drawPolygon(ctx: CanvasRenderingContext2D, vertices: Point[]) {
+	ctx.beginPath();
+	ctx.moveTo(vertices[0][0], vertices[0][1]);
+	for (let i = 1; i < vertices.length; i++) {
+		ctx.lineTo(vertices[i][0], vertices[i][1]);
+	}
 }
